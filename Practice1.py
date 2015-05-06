e57964d07a9453c9ffb4383fc70bb5d9cac99a00
AnaMarcelaV <15avalenzuela@iaa.edu.ec>
2015-05-06 17:44:06 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,15 @@
+name = "Ana Marcela"
+lastname = "Valenzuela"
+adress = "Cdl. Central Park, P-1, Samborondon"
+email = "15avalenzuela@iaa.edu.ec"
+phonenumber = "593 958871657"
+
+print name
+print lastname
+print adress
+print email
+print phonenumber
+
+print 'My name is', name, lastname
+print 'I live in:', adress
+print 'You can contact me at my phone number:', phonenumber, 'or email me at:', email
