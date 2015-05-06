4fca52f33d6fb3554a828eae1ce0709c172afc03
AnaMarcelaV <15avalenzuela@iaa.edu.ec>
2015-05-06 17:43:27 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print "Hello World"
+print "My name is Marcela"
+print "I am 17 years old \nThis is Python"
+
+print 2015 + 1 #Printing an integer
+print "2015"
+
+print 3.1416 #Printing a floating point number
+
+print 14 + 36 #Adding two numbers
+print 100 - 74 #Substracting two numbers
+print 56*5 #Multiplying two numbers
+print 45/5 #Dividing two numbers
+
+#Modulus
+print 10%3
+
+print "Odd or Even Numbers"
+print 17 % 3
+print 16 % 2
+
