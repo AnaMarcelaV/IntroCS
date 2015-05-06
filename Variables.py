644c3aa4f22d03469bcd76e53555deb05ef214ce
AnaMarcelaV <15avalenzuela@iaa.edu.ec>
2015-05-06 17:44:38 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,33 @@
+name = "Marcela Valenzuela" #string 
+age = 17 #integer
+gpa = 4.0 #floating point
+
+print name
+print age 
+print gpa
+
+print 'My name is', name, '. I am ', age, 'years old . My gpa is', gpa
+
+print 'In ten years, I will be', age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7
+
+print 'My dog is', ageInDogYears, 'years old in Dog Years.'
+print 'But my dog is', ageInHumanYears, 'years old in Human Years.'
+
+#order of opperations
+#parentheis, Exponents, Multiplication, Divison, Addition, Substarction
+
+myNumber = 5 * 4 + 10
+
+print myNumber
+
+print "Hello",  2
+
+
+import math
+root = math.sqrt(81)
+print root
+
