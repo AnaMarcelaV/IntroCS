3ca50ea26fce3daff36e7f379e908bf193e646e8
AnaMarcelaV <15avalenzuela@iaa.edu.ec>
2015-05-06 17:47:06 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,12 @@
+operant1 = 95
+operant2 = 64.95
+
+print operant1 + operant2 #Addition of operant1 and operant2
+print operant1 - operant2 #Substarction of operant1 and operant2
+print operant1*operant2 #Multiplication of operant1 and operant2
+print operant1/operant2 #Division of operant1 and operant2
+print operant1%operant2 #Modulus of of operant1 and operant2
+
+#fisrt create the variable of each operant. (operant1 and operant2)
+#Second display all the diffrent opperations
+#print the opperation
